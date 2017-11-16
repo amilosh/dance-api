51b1440896cb79a9abfbcad52ab47d8ce601aa62
Alexander Milosh <milosh.alexander.by@gmail.com>
2017-11-16 03:31:24 +0300

    4. Add limitation.

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 1. Initialization of project.
 2. Add entities structure.
 3. Add logic by step number definition.
+4. Add limitation.
--- src/main/java/by/milosh/api/Platform.java
+++ src/main/java/by/milosh/api/Platform.java
@@ -2,12 +2,19 @@ package by.milosh.api;
 
 public class Platform {
 
+    public static final int MIN_VALUE = 2;
+    public static final int MAX_VALUE = 49;
+
     private int[] sequenceNumber;
 
     public Platform() {
     }
 
-    public Platform(int[] sequenceNumber) {
+    public Platform(int[] sequenceNumber) throws NumberFormatException {
+        int size = sequenceNumber.length;
+        if ((size < MIN_VALUE) || (size > MAX_VALUE)) {
+            throw new NumberFormatException("Sequence number size value out of range. It must be between " + MIN_VALUE +  " and" + MAX_VALUE + " inclusive.");
+        }
         this.sequenceNumber = sequenceNumber;
     }
 
@@ -15,7 +22,11 @@ public class Platform {
         return sequenceNumber;
     }
 
-    public void setSequenceNumber(int[] sequenceNumber) {
+    public void setSequenceNumber(int[] sequenceNumber) throws NumberFormatException{
+        int size = sequenceNumber.length;
+        if ((size < MIN_VALUE) || (size > MAX_VALUE)) {
+            throw new NumberFormatException("Sequence number size value out of range. It must be between " + MIN_VALUE +  " and " + MAX_VALUE + " inclusive.");
+        }
         this.sequenceNumber = sequenceNumber;
     }
 }
